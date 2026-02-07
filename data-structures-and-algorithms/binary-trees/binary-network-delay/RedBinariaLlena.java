1e1e1c1ded9e5012e677ab8ebbb1cda90540b6e9
Camila de la Roza <camila.delaroza217694@alumnos.info.unlp.edu.ar>
2026-02-07 12:10:36 -0300

    Add description

--- data-structures-and-algorithms/binary-trees/binary-network-delay/RedBinariaLlena.java
+++ data-structures-and-algorithms/binary-trees/binary-network-delay/RedBinariaLlena.java
@@ -1,3 +1,16 @@
+/*
+ * Ejercicio: Red Binaria Llena
+ *
+ * Se modela una red binaria llena donde cada nodo representa un retardo
+ * de reenvío expresado en segundos.
+ *
+ * El objetivo es calcular el mayor retardo posible en un camino desde
+ * la raíz hasta una hoja.
+ *
+ * La solución se implementa mediante un recorrido recursivo que acumula
+ * los retardos y selecciona el máximo entre los caminos disponibles.
+ */
+
 package tp2.ejercicio4;
 
 import tp2.ejercicio2.BinaryTree;
